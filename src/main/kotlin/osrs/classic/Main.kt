7680df9b50b4808887d1ca4215dfd93e93001881
zeruth <therealnull@gmail.com>
2022-01-30 15:22:30 -0500

    lock to applet size

--- src/main/kotlin/osrs/classic/Main.kt
+++ src/main/kotlin/osrs/classic/Main.kt
@@ -10,13 +10,15 @@ class Main {
         @JvmStatic
         fun main(args: Array<String>) {
             AppletConfiguration.init()
+            Applet.init()
             val frame = JFrame("OSRS Classic")
-            frame.size = Dimension(1280, 720)
+            frame.size = Applet.applet.size
+            frame.isResizable = false
             val panel = JPanel()
             panel.size = frame.size
-            frame.add(panel)
+            frame.add(panel, null)
             panel.layout = BorderLayout()
-            Applet.init()
+            Applet.applet.size = frame.size
             panel.add(Applet.applet, BorderLayout.CENTER)
             frame.show()
             Applet.applet.init()
